f38eb8314f3513797f95c2bd3662d100fad2976a
juakiCarbonell <joachim.carbonell@googlemail.com>
2021-02-24 10:26:17 +0100

    project set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div >
+      <h2>Portfolio project</h2>
+    </div>
+  );
+}
+
+export default App;

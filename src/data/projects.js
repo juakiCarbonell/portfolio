ee66ed1d838bd4a0059816dbd6a21b6dc329045d
juakiCarbonell <joachim.carbonell@googlemail.com>
2021-03-02 09:42:03 +0100

    extract data

---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: 1,
+    title: 'City Guide',
+    description: 'This is an awesome project',
+    img: 'city-guide-app.png',
+  },
+  {
+    id: 2,
+    title: 'Mern Blog',
+    description: 'This is an awesome project',
+    img: 'mern-blog.png',
+  },
+  {
+    id: 3,
+    title: 'Netflix',
+    description: 'This is an awesome project',
+    img: 'netflix.png',
+  },
+  {
+    id: 4,
+    title: 'Portflio',
+    description: 'This is an awesome project',
+    img: 'portfolio.png',
+  },
+];
+
+export default projects;

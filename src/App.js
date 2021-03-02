d0a57cce1758c47fc81342d53d25b8a39f8a9a1e
juakiCarbonell <joachim.carbonell@googlemail.com>
2021-03-02 21:54:13 +0100

    testimonials added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import About from './components/About';
 import Services from './components/Services';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Testimonials from './components/Testimonials';
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       <Services />
       <Experience />
       <Projects />
+      <Testimonials />
     </>
   );
 }
